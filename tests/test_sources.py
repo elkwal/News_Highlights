2af31e36851292c03bbb7a4f4e83653dc7c92fa3
Elukwal <elukwal3@gmail.com>
2018-10-08 10:43:30 +0300

    add the function setup

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -6,3 +6,10 @@ class SourcesTest(unittest.TestCase):
     """
     Test case to test the behavior of the Sources class
     """
+
+    def setUp(self):
+        """
+        Setup function that will run before every test
+        """
+        self.new_source = Sources('newsbyGatetee', 'My News', 'get the latest updates', 'https://google.com', 'general',
+                                  'ke')
