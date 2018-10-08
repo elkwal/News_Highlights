8408c98ac3ec1e90b493733caa268aa4f175e03b
Elukwal <elukwal3@gmail.com>
2018-10-08 11:30:00 +0300

    add the method global to save the base urls

--- app/request.py
+++ app/request.py
@@ -13,6 +13,3 @@ base_url_articles=None
 
 def configure_request(app):
    global api_key,base_url_source,base_url_articles
-   api_key = app.config['NEWS_API_KEY']
-   base_url_source= 'https://newsapi.org/v2/sources?apiKey=89fe8fe3d6ee4ea89aa68bfb28eeb543'
-   base_url_articles=app.config['ARTICLES_BASE_URL']
