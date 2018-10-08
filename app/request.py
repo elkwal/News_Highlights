72e1ca63320a9505ef6d9e2c3c079bc3623f635e
Elukwal <elukwal3@gmail.com>
2018-10-08 11:32:53 +0300

    add the get_articles function

--- app/request.py
+++ app/request.py
@@ -35,3 +35,31 @@ def get_sources():
            print(source_results)
 
    return source_results
+
+
+def process_results(source_list):
+
+   source_results = []
+   for source_item in source_list:
+       id = source_item.get('id')
+       name = source_item.get('name')
+       source_object = Source(id,name)
+       source_results.append(source_object)
+   return source_results
+def get_articles(id):
+   '''
+   Function that gets the json response to our url request
+   '''
+
+
+   get_articles_url = base_url_articles.format(id,api_key)
+   with urllib.request.urlopen(get_articles_url) as url:
+       get_articles_data = url.read()
+       get_articles_response = json.loads(get_articles_data)
+       articles_results = None
+
+       if get_articles_response['articles']:
+           articles_results_list = get_articles_response['articles']
+           articles_results = process_article_results(articles_results_list)
+
+   return articles_results
