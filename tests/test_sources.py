2961d7fcfd425f98e8054faf488b91b5ec99a7a3
Elukwal <elukwal3@gmail.com>
2018-10-08 10:42:42 +0300

    add the class Sourcestest

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -6,24 +6,3 @@ class SourcesTest(unittest.TestCase):
     """
     Test case to test the behavior of the Sources class
     """
-
-    def setUp(self):
-        """
-        Setup function that will run before every test
-        """
-        self.new_source = Sources('newsbyGatetee', 'My News', 'get the latest updates', 'https://google.com', 'general',
-                                  'ke')
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_source, Source))
-
-    def test_to_check_instance_variables(self):
-        """
-        Test function to check instance variables
-        """
-        self.assertEquals(self.new_source.id, 'newsbyelkwal')
-        self.assertEquals(self.new_source.name, 'My News')
-        self.assertEquals(self.new_source.description, 'get  the latest updates')
-        self.assertEquals(self.new_source.url, 'https://google.com')
-        self.assertEquals(self.new_source.category, 'general')
-        self.assertEquals(self.new_source.country, 'kenya')
