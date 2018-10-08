03b1b15604d12549ef2753c87b4d326a0ba48c5c
Elukwal <elukwal3@gmail.com>
2018-10-08 11:28:23 +0300

    add the variables set to none

--- app/request.py
+++ app/request.py
@@ -4,3 +4,9 @@ from .models import Source,Articles
 
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+
+api_key=None
+
+base_url_source=None
+
+base_url_articles=None
